fcf1632c93316344a3007d2163d326a4097a234b
dguarnizo <guarnizoduvan5@gmail.com>
2022-11-10 12:00:30 -0500

    carga de permisoRoles

--- BackendSeguridad/src/main/java/tutorial/misionTIC/seguridad/Modelos/Rol.java
+++ BackendSeguridad/src/main/java/tutorial/misionTIC/seguridad/Modelos/Rol.java
@@ -1,9 +1,12 @@
 package tutorial.misionTIC.seguridad.Modelos;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document
 public class Rol {
-
     @Id
     private String _id;
 
