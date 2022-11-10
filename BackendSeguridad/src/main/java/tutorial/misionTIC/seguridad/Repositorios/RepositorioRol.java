8f332835add5fdd0a8474b72ec1465c14ac0fa25
Diana Andrade <dpandrader@gmail.com>
2022-11-10 11:40:16 -0500

    commit de permisos

--- BackendSeguridad/src/main/java/tutorial/misionTIC/seguridad/Repositorios/RepositorioRol.java
+++ BackendSeguridad/src/main/java/tutorial/misionTIC/seguridad/Repositorios/RepositorioRol.java
@@ -3,5 +3,5 @@ package tutorial.misionTIC.seguridad.Repositorios;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import tutorial.misionTIC.seguridad.Modelos.Rol;
 
-public interface RepositorioRol extends MongoRepository<Rol, String> {
+public interface RepositorioRol extends MongoRepository<Rol,String> {
 }
